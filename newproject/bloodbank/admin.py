d15cdce9d7af021c8e9917294f490503ad96bb88
Muhammed Midlaj <104316889+muhammedmidlajtv@users.noreply.github.com>
2023-09-08 13:45:13 +0530

    Delete newproject directory

--- newproject/bloodbank/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import BloodDonor
-# Register your models here.
-
-admin.site.register(BloodDonor)
--- newproject/bloodbank/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import BloodDonor
-
-class BloodDonorForm(forms.ModelForm):
-    class Meta:
-        model = BloodDonor
-        fields = ['name', 'blood_type', 'phone']
